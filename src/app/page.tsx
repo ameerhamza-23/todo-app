4e24d2eb7f2051d8d9d79da2e49c84119bb754b2
ameerhamza-23 <hamzaiscoding@gmail.com>
2024-06-20 22:05:12 +0500

    Integrated local storage functionality in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,38 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import { Reorder, useDragControls } from "framer-motion";
 import { Item } from "@/components/Item";
+import { getTasksFromLocalStorage, saveTasksToLocalStorage } from "@/lib/localStorage";
 
 export default function Home() {
   const controls = useDragControls();
   const [items, setItems] = useState<string[]>([]);
   const [current, setCurrent] = useState("");
 
+  useEffect(() => {
+    const localTasks = getTasksFromLocalStorage();
+    setItems(localTasks);
+  },[])
+
+  const addItem = (item:string) => {
+    const newItems = [...items, item]
+    setItems(newItems)
+    saveTasksToLocalStorage(newItems)
+  }
+
+  const removeItem = (itemToRemove:string) => {
+    const newItems = items.filter(item => item !== itemToRemove)
+    setItems(newItems)
+    saveTasksToLocalStorage(newItems)
+  }
+
   const handleClick = () => {
     if (current.trim()) {
-      setItems((prevItems) => [...prevItems, current]);
+      addItem(current)
       setCurrent(""); // Clear the input field after adding the item
     }
   };
